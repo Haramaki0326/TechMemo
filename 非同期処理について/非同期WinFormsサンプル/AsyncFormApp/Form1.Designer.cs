4d01688b1b2ffd0ded5d0d6f5cfb7bc1f6d6284c
Haramaki0326 <blucheeee@outlook.com>
2020-01-11 12:59:04 +0900

    [add]AsyncFormAppを追加

---
+++ 非同期処理について/非同期WinFormsサンプル/AsyncFormApp/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace AsyncFormApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 28);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(208, 55);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "A1\r\n逐次処理・同期処理による重い処理\r\nUIは停止";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(354, 124);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(188, 19);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(59, 295);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(114, 35);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "B1\r\nテキストBに反映";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(59, 347);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(114, 33);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "B2\r\nテキストBに反映";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(354, 327);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(188, 19);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(299, 127);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(49, 12);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "テキストA";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(299, 330);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(49, 12);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "テキストB";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(12, 107);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(208, 53);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "A2\r\n並列処理・非同期処理による重い処理\r\nUIは停止しない";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_ClickAsync);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(12, 178);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(208, 53);
+			this.button5.TabIndex = 8;
+			this.button5.Text = "A3\r\n並列処理・同期処理による重い処理\r\nUIは停止";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+	}
+}
+
---
+++ 非同期処理について/非同期WinFormsサンプル/AsyncFormApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncFormApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//逐次処理・同期処理による重い処理
+			//UIスレッドは停止
+			Thread.Sleep(4560);
+			this.textBox1.Text = "A1_Sync Pushed!";
+		}
+
+		private async void button4_ClickAsync(object sender, EventArgs e)
+		{
+			//並列処理・非同期処理による重い処理
+			Task<int> task = Task.Run<int>(() =>
+			{
+				int total = 0;
+				for (int i = 1; i <= 200; ++i)
+					total += i;
+				Thread.Sleep(4560); // 何か重い処理をしている...
+				return total;
+			});
+
+			//awaitを使用しているのでUIスレッドは停止しない
+			int result = await task; // スレッドの処理の結果を「待ち受け」する
+			this.textBox1.Text = result.ToString();
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			//並列処理・同期処理による重い処理
+			Task<int> task = Task.Run<int>(() =>
+			{
+				int total = 0;
+				for (int i = 1; i <= 100; ++i)
+					total += i;
+				Thread.Sleep(4560); // 何か重い処理をしている...
+				return total;
+			});
+
+			//ハードウェイティングのTask.Resultを使用しているのでUIスレッドは停止する
+			int result = task.Result; // スレッドの処理の結果を「待ち受け」する
+			this.textBox1.Text = result.ToString();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			this.textBox2.Text = "B1 Pushed!";
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			this.textBox2.Text = "B2 Pushed!";
+		}
+	}
+}
